ba193777e6b148f85ee07e39b72492330f13961b
computersunplugged <164129504+computersunplugged@users.noreply.github.com>
2025-11-13 08:43:46 +1100

    Add files via upload

---
+++ Modules/CippExtensions/Public/NinjaOne/New-VulnCsvBytes.ps1
@@ -0,0 +1,17 @@
+function New-VulnCsvBytes {
+    [CmdletBinding()]
+    param([object[]] $Rows,[string[]] $Headers)
+    $sb = New-Object System.Text.StringBuilder
+    [void]$sb.AppendLine(($Headers -join ','))
+    foreach ($r in $Rows) {
+        $cells = foreach ($h in $Headers) {
+            $val = $r.$h; if ($null -ne $val) {
+                $s = [string]$val
+                if ($s -match '[,"
+]') { '"' + ($s -replace '"','""') + '"' } else { $s }
+            } else { '' }
+        }
+        [void]$sb.AppendLine(($cells -join ','))
+    }
+    return [System.Text.Encoding]::UTF8.GetBytes($sb.ToString())
+}
