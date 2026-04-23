da2c211af2192eba4dfbc4f7bd94e7e2acdf68da
computersunplugged <164129504+computersunplugged@users.noreply.github.com>
2026-04-23 11:53:05 +1000

    Update Push-NinjaOneQueue.ps1

--- Modules/CippExtensions/Public/NinjaOne/Push-NinjaOneQueue.ps1
+++ Modules/CippExtensions/Public/NinjaOne/Push-NinjaOneQueue.ps1
@@ -10,10 +10,7 @@ function Push-NinjaOneQueue {
         'AutoMapTenant'    { Invoke-NinjaOneOrgMappingTenant -QueueItem $Item }
         'SyncTenant'       { Invoke-NinjaOneTenantSync -QueueItem $Item }
         'SyncTenants'      { Invoke-NinjaOneSync }
-<<<<<<< HEAD
-=======
         'CveSyncTenant'    { Invoke-NinjaOneCveSyncTenant -QueueItem $Item }
->>>>>>> 10692946b625681c5668488605d72840918772a2
     }
     return $true
 }
