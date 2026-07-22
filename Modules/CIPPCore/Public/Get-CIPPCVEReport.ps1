ad46d6a64c58145e6c82c71a469c813666364f65
computersunplugged <dmat@computersunplugged.com.au>
2026-07-22 12:24:48 +1000

    Update Get-CIPPCVEReport.ps1

--- Modules/CIPPCore/Public/Get-CIPPCVEReport.ps1
+++ Modules/CIPPCore/Public/Get-CIPPCVEReport.ps1
@@ -14,165 +14,165 @@ function Get-CIPPCVEReport {
     #>
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory = $true)][string]$TenantFilter,
-        [Parameter(Mandatory = $true)][string]$UseReportDB
+        [Parameter(Mandatory = $true)]
+        [string]$TenantFilter,
+        [Parameter(Mandatory = $true)]
+        [string]$UseReportDB
     )
 
     try {
         # Retrieve Exceptions from Exception database
         $CveExceptionsTable = Get-CIPPTable -TableName 'CveExceptions'
-        $AllExceptions      = Get-CIPPAzDataTableEntity @CveExceptionsTable
-        $ExceptionsByCve    = @{}
-        $CveMasterTable     = @{}
+        $AllExceptions = Get-CIPPAzDataTableEntity @CveExceptionsTable
+        $ExceptionsByCve = @{}
 
-        # Retrieve CVEs from database
+         # Retrieve CVEs from database
         if ($UseReportDB -eq 'true'){
-            $RawCveData    = Get-CIPPDbItem -TenantFilter 'allTenants' -Type 'DefenderCVEs' | Where-Object { $_.RowKey -ne 'DefenderCVEs-Count' }
+            $RawCveData    = Get-CIPPDbItem -TenantFilter $TenantFilter -Type 'DefenderCVEs' | Where-Object { $_.RowKey -ne 'DefenderCVEs-Count' }
             $AllCachedCves = $RawCveData.Data | ConvertFrom-Json
         } else {
             $AllCachedCves = get-DefenderCVEs -TenantFilter $TenantFilter
         }
 
-        # Filter results by tenant
+        # Filter results by Tenant
+        $RawCveItems = [System.Collections.Generic.List[object]]::new()
+
         if ($TenantFilter -eq 'AllTenants') {
             # Validate against active tenants to ensure we don't return orphaned data
             $TenantList = Get-Tenants -IncludeErrors
-        } else {
-            $TenantList = Get-Tenants | Where-Object defaultDomainName -eq $TenantFilter
-        }
-
-        # Filter CVEs by tenant and build CVE items
-        foreach ($Item in $AllCachedCves) {
-            if ($TenantList.defaultDomainName -contains $Item.customerId) {
-
-                $CveId = $Item.PartitionKey
-
-                if (-not $CveMasterTable.ContainsKey($CveId)) {
-                    $CveMasterTable[$CveId] = @{
-                        cveId                      = $CveId
-                        vulnerabilitySeverityLevel = $Item.vulnerabilitySeverityLevel
-                        exploitabilityLevel        = $Item.exploitabilityLevel
-                        softwareName               = $Item.softwareName
-                        softwareVendor             = $Item.softwareVendor
-                        softwareVersion            = $Item.softwareVersion
-                        lastUpdated                = $Item.lastUpdated
-                        TotalDeviceCount           = 0
-                        AffectedTenantsList        = [System.Collections.Generic.List[object]]::new()
-                        AffectedDevicesList        = [System.Collections.Generic.List[object]]::new()
-                        DiskPathList               = [System.Collections.Generic.List[object]]::new()
-                        RegistryPathList           = [System.Collections.Generic.List[object]]::new()
-                        ExceptionMatchCount        = 0
-                        TotalTenantGroupCount      = 0
-                    }
-                }
-
-                $CveGroup = $CveMasterTable[$CveId]
-                $CveGroup.TotalTenantGroupCount++
-
-                [void]$CveGroup.AffectedTenantsList.Add(@{ customerId = $Item.customerId })
-
-                # Unpack the device JSON details from the row
-                if ($Item.deviceDetailsJson) {
-                    $Devices = ConvertFrom-Json $Item.deviceDetailsJson | Sort-Object -Property deviceName -Unique
-                    foreach ($Dev in $Devices) {
-                            [void]$CveGroup.AffectedDevicesList.Add(@{ deviceName    = $Dev.deviceName })
-                            if($Dev.registryPaths){[void]$CveGroup.RegistryPathList.Add(@{ deviceName = $Dev.deviceName
-                                                                                        registryPaths = $Dev.registryPaths })}
-                            if($Dev.diskPaths){[void]$CveGroup.DiskPathList.Add(@{ deviceName = $Dev.deviceName
-                                                                                diskPaths = $Dev.diskPaths })}
-                            $CveGroup.TotalDeviceCount ++
-                    }
+            foreach ($Item in $AllCachedCves) {
+                if ($TenantList.defaultDomainName -contains $Item.customerId) {
+                    [void]$RawCveItems.Add($Item)
                 }
             }
         }
+        else {
+            $TenantList = Get-Tenants -TenantFilter $TenantFilter
+            $RawCveItems.AddRange(@($AllCachedCves))
+        }
 
-        # Filter exceptions by tenant and build exception items
+        if ($RawCveItems.Count -eq 0) {
+            return @()
+        }
 
-        foreach ($Ex in $AllExceptions){
+        # Build filtered exception items
+        foreach ($Ex in $AllExceptions) {
+            if ($TenantList.defaultDomainName -contains $Ex.customerId -or $Ex.customerId -eq 'ALL') {
+                if (-not $ExceptionsByCve.ContainsKey($Ex.cveId)) {
+                    $ExceptionsByCve[$Ex.cveId] = [System.Collections.Generic.List[object]]::new()
+                }
 
-                if ($TenantList.defaultDomainName -contains $Ex.customerId -or $Ex.customerId -eq 'ALL'){
-                    $CveId = $Ex.cveId
+                [void]$ExceptionsByCve[$Ex.cveId].Add([PSCustomObject]@{
+                        cveId              = $Ex.cveId
+                        customerId         = $Ex.customerId
+                        exceptionSource    = $Ex.exceptionSource
+                        exceptionType      = @{ customerId = $Ex.customerId
+                                                exceptionType = $Ex.exceptionType }
+                        exceptionComment   = @{ customerId = $Ex.customerId
+                                                exceptionComment = $Ex.exceptionComment }
+                        exceptionCreatedBy = @{ customerId = $Ex.customerId
+                                                exceptionCreatedBy = $Ex.exceptionCreatedBy }
+                        exceptionDate      = @{ customerId = $Ex.customerId
+                                                exceptionDate = $Ex.exceptionReadableDate }
+                        exceptionExpiry    = @{ customerId = $Ex.customerId
+                                                exceptionExpiry = $Ex.exceptionExpiry }
+                    })
+            }
+        }
 
-                    if (-not $ExceptionsByCve.ContainsKey($CveId)) {
-                        $ExceptionsByCve[$CveId] = @{
-                            cveId              = $Ex.cveId
-                            customerId         = $Ex.customerId
-                            exceptionSource    = $Ex.exceptionSource
-                            exceptionType      = [System.Collections.Generic.List[object]]::new()
-                            exceptionComment   = [System.Collections.Generic.List[object]]::new()
-                            exceptionCreatedBy = [System.Collections.Generic.List[object]]::new()
-                            exceptionDate      = [System.Collections.Generic.List[object]]::new()
-                            exceptionExpiry    = [System.Collections.Generic.List[object]]::new()
-                        }
-                        [void]$ExceptionsByCve[$CveId].exceptionType.Add(@{ customerId = $Ex.customerId
-                                                                    exceptionType = $Ex.exceptionType })
-                        [void]$ExceptionsByCve[$CveId].exceptionComment.Add(@{ customerId = $Ex.customerId
-                                                                    exceptionComment = $Ex.exceptionComment })
-                        [void]$ExceptionsByCve[$CveId].exceptionCreatedBy.Add(@{ customerId = $Ex.customerId
-                                                                    exceptionCreatedBy = $Ex.exceptionCreatedBy })
-                        [void]$ExceptionsByCve[$CveId].exceptionDate.Add(@{ customerId = $Ex.customerId
-                                                                    exceptionDate = $Ex.exceptionReadableDate })
-                        [void]$ExceptionsByCve[$CveId].exceptionExpiry.Add(@{ customerId = $Ex.customerId
-                                                                    exceptionExpiry = $Ex.exceptionExpiry })
-                    } else {
-                        # Handle duplicate exceptions
-                        $ExceptionsByCve[$CveId].customerId         = $ExceptionsByCve[$CveId].customerId,$Ex.customerId
-                        $ExceptionsByCve[$CveId].exceptionSource    = $ExceptionsByCve[$CveId].exceptionSource,$Ex.exceptionSource
-                        [void]$ExceptionsByCve[$CveId].exceptionType.Add(@{ customerId = $Ex.customerId
-                                                                    exceptionType = $Ex.exceptionType })
-                        [void]$ExceptionsByCve[$CveId].exceptionComment.Add(@{ customerId = $Ex.customerId
-                                                                    exceptionComment = $Ex.exceptionComment })
-                        [void]$ExceptionsByCve[$CveId].exceptionCreatedBy.Add(@{ customerId = $Ex.customerId
-                                                                    exceptionCreatedBy = $Ex.exceptionCreatedBy })
-                        [void]$ExceptionsByCve[$CveId].exceptionDate.Add(@{ customerId = $Ex.customerId
-                                                                    exceptionDate = $Ex.exceptionReadableDate })
-                        [void]$ExceptionsByCve[$CveId].exceptionExpiry.Add(@{ customerId = $Ex.customerId
-                                                                    exceptionExpiry = $Ex.exceptionExpiry })
+        # Process raw CVE items
+        $CveMasterTable = @{}
+
+        foreach ($Item in $RawCveItems) {
+            $CveId = $Item.PartitionKey
+
+            if (-not $CveMasterTable.ContainsKey($CveId)) {
+                $CveMasterTable[$CveId] = @{
+                    cveId                      = $CveId
+                    vulnerabilitySeverityLevel = $Item.vulnerabilitySeverityLevel
+                    exploitabilityLevel        = $Item.exploitabilityLevel
+                    softwareName               = $Item.softwareName
+                    softwareVendor             = $Item.softwareVendor
+                    softwareVersion            = $Item.softwareVersion
+                    lastUpdated                = $Item.lastUpdated
+                    TotalDeviceCount           = 0
+                    AffectedTenantsList        = [System.Collections.Generic.List[object]]::new()
+                    AffectedDevicesList        = [System.Collections.Generic.List[object]]::new()
+                    DiskPathList               = [System.Collections.Generic.List[object]]::new()
+                    RegistryPathList           = [System.Collections.Generic.List[object]]::new()
+                    ExceptionMatchCount        = 0
+                    TotalTenantGroupCount      = 0
+                    ExceptionSources           = [System.Collections.Generic.HashSet[string]]::new()
                 }
             }
+
+            $CveGroup = $CveMasterTable[$CveId]
+            $CveGroup.TotalTenantGroupCount++
+
+            [void]$CveGroup.AffectedTenantsList.Add(@{ customerId = $Item.customerId })
+
+            # Unpack the device JSON details from the row
+            if ($Item.deviceDetailsJson) {
+                $Devices = ConvertFrom-Json $Item.deviceDetailsJson | Sort-Object -Property deviceName -Unique
+                foreach ($Dev in $Devices) {
+                    [void]$CveGroup.AffectedDevicesList.Add(@{ deviceName = $Dev.deviceName })
+                    if ($Dev.registryPaths) {
+                        [void]$CveGroup.RegistryPathList.Add(@{ deviceName = $Dev.deviceName
+                                registryPaths                              = $Dev.registryPaths
+                            })
+                    }
+                    if ($Dev.diskPaths) {
+                        [void]$CveGroup.DiskPathList.Add(@{ deviceName = $Dev.deviceName
+                                diskPaths                              = $Dev.diskPaths
+                            })
+                    }
+                    $CveGroup.TotalDeviceCount ++
+                }
             }
+        }
 
         # Combine filtered results
         $SortedCves = [System.Collections.Generic.List[PSCustomObject]]::new()
 
         foreach ($CveKey in $CveMasterTable.Keys) {
-            $Target          = $CveMasterTable[$CveKey]
-            $Exceptions      = $ExceptionsByCve[$CveKey]
+            $Target = $CveMasterTable[$CveKey]
+            $Exceptions = $ExceptionsByCve[$CveKey]
             $ExceptionStatus = 'None'
-            $HasException    = $false
+            $HasException = $false
 
-            if ($Exceptions){
-                $HasException       = $true
-                $ExceptionStatus    = if ($Exceptions.customerId -contains "ALL") { "All" } else { "Partial" }
+            if ($Exceptions) {
+                $HasException = $true
+                $ExceptionStatus = if ($Exceptions.customerId -contains "ALL") { "All" } else { "Partial" }
             }
 
             [void]$SortedCves.Add([PSCustomObject]@{
-                cveId                      = $Target.cveId
-                vulnerabilitySeverityLevel = $Target.vulnerabilitySeverityLevel
-                exploitabilityLevel        = $Target.exploitabilityLevel
-                softwareName               = $Target.softwareName
-                softwareVendor             = $Target.softwareVendor
-                softwareVersion            = $Target.softwareVersion
-                deviceCount                = $Target.TotalDeviceCount
-                tenantCount                = $Target.TotalTenantGroupCount
-                registryPaths              = $Target.RegistryPathList
-                diskPaths                  = $Target.DiskPathList
-                exceptionStatus            = $ExceptionStatus
-                hasException               = $HasException
-                affectedTenants            = $Target.AffectedTenantsList
-                affectedDevices            = $Target.AffectedDevicesList
-                exceptionType              = $Exceptions.exceptionType ?? ''
-                exceptionComment           = $Exceptions.exceptionComment ?? ''
-                exceptionCreatedBy         = $Exceptions.exceptionCreatedBy ?? ''
-                exceptionDate              = $Exceptions.exceptionDate ?? ''
-                exceptionExpiry            = $Exceptions.exceptionExpiry ?? ''
-                cacheTimeStamp             = $Target.lastUpdated
-            })
+                    cveId                      = $Target.cveId
+                    vulnerabilitySeverityLevel = $Target.vulnerabilitySeverityLevel
+                    exploitabilityLevel        = $Target.exploitabilityLevel
+                    softwareName               = $Target.softwareName
+                    softwareVendor             = $Target.softwareVendor
+                    softwareVersion            = $Target.softwareVersion
+                    deviceCount                = $Target.TotalDeviceCount
+                    tenantCount                = $Target.TotalTenantGroupCount
+                    registryPaths              = $Target.RegistryPathList
+                    diskPaths                  = $Target.DiskPathList
+                    exceptionStatus            = $ExceptionStatus
+                    hasException               = $HasException
+                    affectedTenants            = $Target.AffectedTenantsList
+                    affectedDevices            = $Target.AffectedDevicesList
+                    exceptionType              = $Exceptions.exceptionType ?? ''
+                    exceptionComment           = $Exceptions.exceptionComment ?? ''
+                    exceptionCreatedBy         = $Exceptions.exceptionCreatedBy ?? ''
+                    exceptionDate              = $Exceptions.exceptionDate ?? ''
+                    exceptionExpiry            = $Exceptions.exceptionExpiry ?? ''
+                    cacheTimeStamp             = $Target.lastUpdated
+                })
         }
 
         return  $SortedCves | Sort-Object -Property cveId
 
-    } catch {
+    }
+    catch {
         Write-LogMessage -API 'CVEReport' -tenant $TenantFilter -message "Failed to generate CVE report: $($_.Exception.Message)" -sev Error
         throw
     }
